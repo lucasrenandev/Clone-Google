d5aa83e35104ac0d59045ed4ab5847a8857052af
Lucas Renan <lucasrenandns96@gmail.com>
2022-09-05 16:31:17 -0300

    Update google.js

---
+++ assets/script/google.js
@@ -0,0 +1,17 @@
+
+const search = document.querySelector("#search")
+const button = document.querySelector("#button")
+
+search.addEventListener("keypress", function(e) {
+    
+    if(e.key === "Enter") {
+        const url = "https://www.google.com/search?q=" + search.value
+        search.click()
+        window.open(url, "_self")
+    }
+})
+
+button.addEventListener("click", () => {
+    const url = "https://www.google.com/search?q=" + search.value
+    window.open(url, "_self")
+})
